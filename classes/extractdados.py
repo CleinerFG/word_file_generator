4d2c012ddcc936480bb5a69e629b590f969f218e
CleinerFG <cleinerfurlani@gmail.com>
2024-11-18 10:15:48 -0300

    Refactor project

--- classes/extractdados.py
+++ classes/extractdados.py
@@ -3,46 +3,50 @@ from typing import Dict, List
 
 class ExtractDados:
     """
-    Classe responsável por extrair os dados da planilha
+    Responsible for extracting data from the sheet.
     """
-    def __init__(self, diretorio: str):
-        self._diretorio = diretorio
-        self._dados = []
+    def __init__(self, dir: str):
+        self._dir = dir
+        self._data = []
+    
+    @property
+    def dir(self) -> str:
+        return self._dir
+
+    @dir.setter
+    def dir(self, value: str):
+        self._dir = value
 
     @property
-    def diretorio(self) -> str:
-        return self._diretorio
+    def data(self) -> List[Dict]:
+        return self._data
+    
+    @data.setter
+    def data(self, value: List[Dict]) -> None:
+        self._data = value
 
-    @diretorio.setter
-    def diretorio(self, value: str):
-        self._diretorio = value
-
-    @property
-    def dados(self) -> List[Dict]:
-        return self._dados
-
-    def _remove_espacos(self) -> List[Dict]:
-        dados_normalizados = []
-        for row in self._dados:
+    def __normalize_data(self) -> List[Dict]:
+        normalized_data = []
+        for row in self._data:
             dict_row = {}
             for k, v in row.items():
                 dict_row[str(k).strip()] = str(v).strip()
-            dados_normalizados.append(dict_row)
-        return dados_normalizados
+            normalized_data.append(dict_row)
+        return normalized_data
 
     def read_sheet(self) -> None:
         try:
-            sheet = pd.read_excel(self._diretorio)
-            self._dados = sheet.to_dict(orient='records')
-            self._dados = self._remove_espacos()
+            sheet = pd.read_excel(self._dir)
+            self.data = sheet.to_dict(orient='records')
+            self.data = self.__normalize_data()
         except FileNotFoundError:
-            print('O diretório para a planilha está incorreto!')
+            print('The directory for the sheet is incorrect!')
 
-    def print_dados(self) -> None:
-        if self._dados:
-            for row in self._dados:
+    def printdata(self) -> None:
+        if self._data:
+            for row in self._data:
                 for k,v in row.items():
                     print(f'{k}: {v}', end=' ')
                 print('\n')
         else:
-            print('Não há dados')
+            print('No data')
